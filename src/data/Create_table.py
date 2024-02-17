ff7422b7132c48ef31bbf671c4e52ad3332d4c6f
Lucio Cravero <craverolucio@gmail.com>
2024-02-17 12:12:29 -0300

    update create_table.py

--- src/data/Create_table.py
+++ src/data/Create_table.py
@@ -32,7 +32,7 @@ conn = engine.connect()
 sql_query = '''
 CREATE TABLE IF NOT EXISTS craverolucio_coderhouse.carbon_intensity (
     from_date VARCHAR(50) PRIMARY KEY,
-    to_date VARCHAR(50),
+    to_date VARCHAR(50) SORTKEY,
     intensity_max SMALLINT,
     intensity_average SMALLINT,
     intensity_min SMALLINT,
